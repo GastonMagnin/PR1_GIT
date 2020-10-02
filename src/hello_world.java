eb8f8d7ab3c3b3031fd23303a40cafa9cfd11801
GastonMagnin <44193118+GastonMagnin@users.noreply.github.com>
2020-10-02 17:07:34 +0200

    Update hello_world.java

--- src/hello_world.java
+++ src/hello_world.java
@@ -1,7 +1,9 @@
 
 public class hello_world {
 	public static void main(String[] args) {
-		// This is a print
+		// This is a print 
 		System.out.println("Hello World");
+		// This is another print
+		System.out.println("Bye World");
 	}
 }

c033caea7f2c2c63b99003a296d4f49578c4d297
GastonMagnin <44193118+GastonMagnin@users.noreply.github.com>
2020-10-02 17:11:28 +0200

    Update hello_world.java

--- src/hello_world.java
+++ src/hello_world.java
@@ -2,7 +2,7 @@
 public class hello_world {
 	public static void main(String[] args) {
 		// This is a print 
-		System.out.println("Hello World");
+		System.out.println("Hello Jeremias");
 		// This is another print
 		System.out.println("Bye World");
 	}
